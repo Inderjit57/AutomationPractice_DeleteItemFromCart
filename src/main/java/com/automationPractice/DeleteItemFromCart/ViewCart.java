d3baec59db3bc27aa4ed0a21d1d95f29557dce02
Inderjit57 <singhinderjit57@yahoo.com>
2022-04-05 06:03:26 -0400

    added second Scenario

--- src/main/java/com/automationPractice/DeleteItemFromCart/ViewCart.java
+++ src/main/java/com/automationPractice/DeleteItemFromCart/ViewCart.java
@@ -5,21 +5,43 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.automationPractice.BasePackage.TestBase;
+import com.automationPractice.Utils.Utils;
 
 public class ViewCart extends TestBase {
 	public ViewCart() {
 		PageFactory.initElements(wd, this);
 	}
 
+	/*
+	 * Scenario : Item to be Deleted from Cart
+	 */
 	@FindBy(css = "a[title='Delete']")
 	WebElement deleteItemFromCart;
 
 	public void clickDelete() {
 		// Close the tab
-		deleteItemFromCart.click();
+		Utils.javascriptClick(deleteItemFromCart);
 	}
 
 	public ShoppingCartSummary goToShoppingCart() {
 		return new ShoppingCartSummary();
 	}
+	
+	/*
+	 * Scenario :Purchase Item
+	 */
+	@FindBy(css = "#total_price_container")
+	WebElement totalPriceDisplayed;
+	
+	@FindBy(css = ".cart_navigation.clearfix a[title='Proceed to checkout'] span i")
+	WebElement proceedToCheckOutBtnOnViewCart;
+	
+	public void getTotalPriceDisplayed() {
+		Utils.getTextFromWebelement(totalPriceDisplayed);
+	}
+	public DeliveryAddress clickOnProceedToCheckout() {
+		Utils.javascriptClick(proceedToCheckOutBtnOnViewCart);
+		return new DeliveryAddress();
+	}
+
 }
