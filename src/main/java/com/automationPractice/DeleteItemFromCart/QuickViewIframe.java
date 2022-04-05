036082262fcc40ce5595cc53984374fb00b1c7fe
Inderjit57 <singhinderjit57@yahoo.com>
2022-04-05 06:00:25 -0400

    Added second Scenario code base

--- src/main/java/com/automationPractice/DeleteItemFromCart/QuickViewIframe.java
+++ src/main/java/com/automationPractice/DeleteItemFromCart/QuickViewIframe.java
@@ -1,21 +1,24 @@
 package com.automationPractice.DeleteItemFromCart;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.automationPractice.BasePackage.TestBase;
+import com.automationPractice.Utils.Utils;
 
 public class QuickViewIframe extends TestBase {
 
 	public QuickViewIframe() {
 		PageFactory.initElements(wd, this);
 	}
+	
+	/*
+	 *  First Scenario : Item to be deleted from Cart. So, the frame is closed
+	 *  to go to Cart
+	 */
 
+	// this element is shared between two scenarios
 	@FindBy(css = ".box-cart-bottom button[type='submit']")
 	WebElement addToCart;
 
@@ -27,32 +30,59 @@ public class QuickViewIframe extends TestBase {
 
 	@FindBy(css = "a[title='View my shopping cart']")
 	WebElement goToCart;
-
+	
+	
 	public void addToCart() {
-		addToCart.click();
-		
+		Utils.javascriptClick(addToCart);
+
 		// switch back to default content
 		wd.switchTo().defaultContent();
 	}
-	
-	//Close the item selection frame
+
+	// Close the item selection frame
 	public void closeTheframe() {
 		closeBtn.click();
 	}
-	
-	// Go to Cart page 
+
+	// Go to Cart page
 	public ViewCart clickOnCart() {
-		actions = new Actions(wd);
-		actions.moveToElement(goToCart).perform();
+		Utils.moveToElement(goToCart);
 
 		goToCart.click();
 		return new ViewCart();
 	}
-	
-	//Return to Women Tab page
+
+	// Return to Women Tab page
 	public WomenTab goToCart() {
 		closeBtn.click();
 		return new WomenTab();
 	}
+
+	/*
+	 * Second Scenario : item to be purchased and proceeded to checkout
+	 */
+
+	@FindBy(css = "#quantity_wanted_p :nth-of-type(2) span i")
+	WebElement addQuantity;
+
+	@FindBy(css = ".attribute_list div select option:nth-of-type(3)")
+	WebElement selectSize;
+
+	@FindBy(css = ".layer_cart_cart.col-xs-12.col-md-6 :nth-of-type(4) a")
+	WebElement checkOutBtn;
+
+	// Using Javascript Executer to click to add quantity
+	public void addQuantity() {
+		Utils.javascriptClick(addQuantity);
+	}
+
+	// Using Javascript Executer to click to select size
+	public void sizeSelection() {
+		Utils.javascriptClick(selectSize);
+	}
+	 public ViewCart proceedToCheckOut() {
+		 checkOutBtn.click();
+		 return new ViewCart();		 
+	 }
 
 }
