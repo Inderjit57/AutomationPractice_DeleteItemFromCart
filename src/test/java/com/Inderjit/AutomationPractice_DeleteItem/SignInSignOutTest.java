b648459171396d3f2c9f19b325cebbc4da42b436
Inderjit57 <singhinderjit57@yahoo.com>
2022-04-08 05:57:41 -0400

    Data reading from Excel in progress

--- src/test/java/com/Inderjit/AutomationPractice_DeleteItem/SignInSignOutTest.java
+++ src/test/java/com/Inderjit/AutomationPractice_DeleteItem/SignInSignOutTest.java
@@ -1,5 +1,8 @@
 package com.Inderjit.AutomationPractice_DeleteItem;
 
+import java.io.IOException;
+import java.util.Iterator;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -9,6 +12,7 @@ import com.automationPractice.BasePackage.TestBase;
 import com.automationPractice.PageClass.AccountPage;
 import com.automationPractice.PageClass.Homepage;
 import com.automationPractice.PageClass.SignInSignOut;
+import com.automationPractice.Utils.ExcelUtils;
 
 public class SignInSignOutTest extends TestBase {
 	Homepage homepage;
@@ -53,5 +57,32 @@ public class SignInSignOutTest extends TestBase {
 	@AfterMethod
 	public void closeBrowser() {
 		tearDown();
+	}
+	
+	// Get the login Credentials from Excel file
+	public void readAndFeebLoginDataFromExcel() throws IOException {
+		// First need to locate the file path
+		String filePath = "";
+		
+		// Locate the Row in a sheet
+		int rows = ExcelUtils.getRowCount(filePath, "Sheet1");
+		
+		// Locate the column to go to a cell
+		int col = ExcelUtils.getCellCount(filePath, "Sheet1", rows);
+		
+		// Create 2 Diamensional Array in the memory of java to fetch data from rows and column
+		String[] names = new String[5];
+		
+		String[][] loginData = new String[rows][col];
+		
+		//Using Loop to fill data in cells
+		// For loop is used to pick Row first
+		for (int i=1;i<rows;i++) {
+			//nested For loop to get data from column in a particular row
+			for (int j=0; j <col ; j++) {
+				loginData[i-1][j] = ExcelUtils.getCellData(filePath, "Sheet1", i, j);
+			}
+			
+		}
 	}
 }
