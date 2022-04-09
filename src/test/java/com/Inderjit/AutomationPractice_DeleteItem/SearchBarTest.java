2460cc52187b963e4b62fb0525a60b3dea984942
Inderjit57 <singhinderjit57@yahoo.com>
2022-04-08 23:14:45 -0400

    Excel is added to transfer Data to testNG test

--- src/test/java/com/Inderjit/AutomationPractice_DeleteItem/SearchBarTest.java
+++ src/test/java/com/Inderjit/AutomationPractice_DeleteItem/SearchBarTest.java
@@ -1,18 +1,24 @@
 package com.Inderjit.AutomationPractice_DeleteItem;
 
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.automationPractice.BasePackage.TestBase;
 import com.automationPractice.PageClass.Homepage;
 import com.automationPractice.PageClass.PageAfterClickingSearchBtn;
 import com.automationPractice.PageClass.SearchBar;
+import com.automationPractice.Utils.ExcelUtils;
 
 public class SearchBarTest extends TestBase {
 	Homepage homepage;
 	PageAfterClickingSearchBtn pageAfterClickingSearchBtn;
+	SoftAssert sf = new SoftAssert();
 
 	@BeforeMethod
 	public void intialisation() {
@@ -23,21 +29,52 @@ public class SearchBarTest extends TestBase {
 
 	}
 
-	@Test
-	public void verifySearchBarIsWorking() {
-
-		homepage.writeIteminSearchBar(properties.getProperty("writeTextInSearchBar"));
-		pageAfterClickingSearchBtn= homepage.clickSearch();
+	@Test(dataProvider = "signUpDataProvider")
+	public void verifySearchBarIsWorking(String text) {
+		
+		homepage.writeIteminSearchBar(text);
+		
+//		homepage.writeIteminSearchBar(properties.getProperty("writeTextInSearchBar"));
+		pageAfterClickingSearchBtn = homepage.clickSearch();
 
 		String searchedContext = pageAfterClickingSearchBtn.getSearchedText();
 		String actualContext = properties.getProperty("actualTextPresentAfterSearching");
-		Assert.assertEquals(searchedContext, actualContext, "Text not present");
+		sf.assertEquals(searchedContext, actualContext, "Text not present");
 		System.out.println("Text displayed after cliking search :" + searchedContext);
+		sf.assertAll();
 	}
 
 	@AfterMethod
 	public void closeBrowser() {
 		tearDown();
+	}
+
+	// Get the login Credentials from Excel file
+	@DataProvider(name = "signUpDataProvider")
+	public String[][] readAndFeebLoginDataFromExcel() throws IOException {
+		// First need to locate the file path
+		String filePath = "C:\\Everything\\Sheet1.xlsx";
+
+		// Locate the Row in a sheet
+		int rows = ExcelUtils.getRowCount(filePath, "Sheet2");
+
+		// Locate the column to go to a cell
+		int col = ExcelUtils.getCellCount(filePath, "Sheet2", rows);
+
+		// Create 2 Diamensional Array in the memory of java to fetch data from rows and
+		// column
+		String[][] loginData = new String[rows][col];
+
+		// Using Loop to fill data in cells
+		// For loop is used to pick Row first
+		for (int i = 1; i < rows; i++) {
+			// nested For loop to get data from column in a particular row
+			for (int j = 0; j < col; j++) {
+				loginData[i - 1][j] = ExcelUtils.getCellData(filePath, "Sheet2", i, j);
+			}
+
+		}
+		return loginData;
 	}
 
 }
